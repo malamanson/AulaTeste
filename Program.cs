016aebd6fc5157accaa9d21bdaf994473ef055d2
Wiliam-Maciel <malamanson@hotmail.com>
2021-04-12 15:02:40 -0300

    Riscos

--- Program.cs
+++ Program.cs
@@ -54,7 +54,7 @@ namespace Aula77
             foreach (string obj in list)
             {
                 Console.WriteLine(obj);
-                Console.WriteLine("Mala Git HOUUUU");
+                Console.WriteLine("Mala Git HOUUUU__________");
             }
         }
     }
